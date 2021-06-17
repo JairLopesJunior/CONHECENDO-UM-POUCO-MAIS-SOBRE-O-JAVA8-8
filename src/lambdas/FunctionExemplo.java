618de654f3c9f9ec33b2e84bf4d627eb4c59af06
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-06-16 23:03:33 -0300

    Aprendendo um pouco mais sobre a Interface Funcional Function

---
+++ src/lambdas/FunctionExemplo.java
@@ -0,0 +1,14 @@
+package lambdas;
+
+import java.util.function.Function;
+
+public class FunctionExemplo {
+
+    public static void main(String[] args) {
+        // A Interface Funcional Function recebe dois parametros, o primeiro é o que ela vai receber como parametro, o segundo  é  o  que  ela vai retornar.
+
+        Function<String, Integer> retorneTamanhoString = ( (String nome) -> nome.length() );
+
+        System.out.println(retorneTamanhoString.apply("Jair"));
+    }
+}
