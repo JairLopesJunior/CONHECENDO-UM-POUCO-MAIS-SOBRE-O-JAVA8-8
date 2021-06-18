84cd4a1c14be273551a4a0f84ba1e1157ef57ba7
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-06-17 23:32:18 -0300

    Aprendendo um pouco sobre o Method Reference arbitrary object of a particular type

--- src/lambdas/MethodReferences.java
+++ src/lambdas/MethodReferences.java
@@ -2,6 +2,9 @@ package lambdas;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import static java.util.Arrays.asList;
 
@@ -23,5 +26,19 @@ public class MethodReferences {
         Collections.sort(carros, ComparadorCarro::comparePorAno);
         System.out.println(carros);
 
+        // 3- Reference to an  instance method of an arbitrary object of a particular type
+        List<String> nomes = asList("Willian", "DevDojo", "Anna", "Brenon");
+        nomes.sort( (n1, n2) -> n1.compareTo(n2) );
+        nomes.sort(String::compareTo);
+        System.out.println(nomes);
+
+        Function<String, Integer> stringToInteger = (String a) -> Integer.parseInt(a);
+        Function<String, Integer> stringToInteger2 = Integer::parseInt;
+        System.out.println(stringToInteger2.apply("10"));
+
+        BiPredicate<List<String>, String> contains = (lista, elemento) -> lista.contains(elemento);
+        BiPredicate<List<String>, String> contains2 = List::contains;
+        System.out.println(contains2.test(nomes, "Willian"));
+        
     }
 }
